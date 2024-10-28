32edb605e7baead2837d43045e7a60ee5afb4668
Himachal <himachalkumar75@gmail.com>
2024-10-28 11:41:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=';
+
+function MovieApp() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    getMovies(API_URL);
+  }, []);
+
+  const getMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovies(data.results);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm('');
+    }
+  };
+
+  const getClassByRate = (vote) => {
+    if (vote >= 8) return 'green';
+    if (vote >= 5) return 'orange';
+    return 'red';
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="movie-app">
+      <header>
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            className="search"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </form>
       </header>
+      <main className="main">
+        {movies.map((movie) => (
+          <div className="movie" key={movie.id}>
+            <img src={IMG_PATH + movie.poster_path} alt={movie.title} />
+            <div className="movie-info">
+              <h3>{movie.title}</h3>
+              <span className={getClassByRate(movie.vote_average)}>{movie.vote_average}</span>
+            </div>
+            <div className="overview">
+              <h3>Overview</h3>
+              <p>{movie.overview}</p>
+            </div>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
 
-export default App;
+export default MovieApp;
